7effd41a70717ed089a6ac47e91e019b72c10ee8
ammarha123 <hawari@graduate.utm.my>
2023-10-13 13:24:55 +0700

    fix: allow the user to select single category and multiple category in analisa data

--- pages/analisa-data/renderer.js
+++ pages/analisa-data/renderer.js
@@ -43,19 +43,6 @@ const toggleCheckboxes = () => {
 document.getElementById('kecamatanDropdown').addEventListener('change', toggleCheckboxes);
 document.getElementById('desaDropdown').addEventListener('change', toggleCheckboxes);
 
-// Add a click event listener to each checkbox
-const checkboxes = document.querySelectorAll('.form-check-input');
-checkboxes.forEach((checkbox) => {
-  checkbox.addEventListener('click', () => {
-    // Uncheck all checkboxes except the clicked one
-    checkboxes.forEach((otherCheckbox) => {
-      if (otherCheckbox !== checkbox) {
-        otherCheckbox.checked = false;
-      }
-    });
-  });
-});
-
 // Function to retrieve Kecamatan data
 const getKecamatan = () => {
   const kecamatanDropdown = document.getElementById('kecamatanDropdown');
@@ -138,260 +125,74 @@ document.getElementById('kecamatanDropdown').addEventListener('change', () => {
 
 // Event listener for the Desa/Kelurahan dropdown change event
 document.getElementById('desaDropdown').addEventListener('change', () => {
-  // Toggle the checkboxes based on the selection
-  toggleCheckboxes();
-});
+  const selectedDesa = document.getElementById('desaDropdown').value;
 
-
-// Add a click event listener to the "namaCheckbox" checkbox
-const namaCheckbox = document.getElementById('namaCheckbox');
-namaCheckbox.addEventListener('click', () => {
-  if (namaCheckbox.checked) {
-    const selectedDesa = document.getElementById('desaDropdown').value;
-
-    // Define the file path based on the selected Kecamatan
-    let filePath = '';
-
-    // Construct the SQL query to fetch the file path for the selected Kecamatan
-    const sql = `SELECT file FROM desa WHERE name = '${selectedDesa}'`;
-
-    // Query your database to get the file path
-    dbConnection.query(sql, (err, rows) => {
-      if (err) {
-        console.error('Error fetching file path:', err);
-        return;
-      }
-
-      print(rows, 1)
-    });
+  if (selectedDesa) {
+    // Call the function to populate the table data based on the selected "Desa"
+    getExcelDataByDesa(selectedDesa);
   } else {
-    // Clear the table when the checkbox is unchecked
-    const tableBody = document.querySelector('#excelDataTable');
-    tableBody.innerHTML = ''; // Remove all table rows
+    // Clear the table when no "Desa" is selected
+    clearTableData();
   }
 });
 
-const jeniskelaminCheckbox = document.getElementById('jeniskelaminCheckbox');
-jeniskelaminCheckbox.addEventListener('click', () => {
-  if (jeniskelaminCheckbox.checked) {
-    const selectedDesa = document.getElementById('desaDropdown').value;
+// Function to populate the table with data based on the selected "Desa"
+const getExcelDataByDesa = (selectedDesa) => {
+  // Define the file path based on the selected "Desa"
+  let filePath = '';
 
-    // Define the file path based on the selected Kecamatan
-    let filePath = '';
+  // Construct the SQL query to fetch the file path for the selected "Desa"
+  const sql = `SELECT file FROM desa WHERE name = ?`;
 
-    // Construct the SQL query to fetch the file path for the selected Kecamatan
-    const sql = `SELECT file FROM desa WHERE name = '${selectedDesa}'`;
+  // Query your database to get the file path
+  dbConnection.query(sql, [selectedDesa], (err, rows) => {
+    if (err) {
+      console.error('Error fetching file path:', err);
+      return;
+    }
 
-    // Query your database to get the file path
-    dbConnection.query(sql, (err, rows) => {
-      if (err) {
-        console.error('Error fetching file path:', err);
-        return;
-      }
+    // Assuming you have only one file path per "Desa"
+    if (rows.length > 0) {
+      const fileRow = rows[0];
+      filePath = `../../database/${fileRow.file}`;
 
-      print(rows, 2)
-    });
-  } else {
-    // Clear the table when the checkbox is unchecked
-    const tableBody = document.querySelector('#excelDataTable');
-    tableBody.innerHTML = ''; // Remove all table rows
-  }
-});
+      // Fetch and load the Excel file
+      fetch(filePath)
+        .then((response) => response.blob()) // Get the Excel file as a blob
+        .then((blob) => {
+          const reader = new FileReader();
+          reader.onload = (e) => {
+            const data = e.target.result;
+            const workbook = XLSX.read(data, { type: 'binary' });
 
-const usiaCheckbox = document.getElementById('usiaCheckbox');
-usiaCheckbox.addEventListener('click', () => {
-  if (usiaCheckbox.checked) {
-    const selectedDesa = document.getElementById('desaDropdown').value;
+            // Assuming you have only one sheet in the Excel file
+            const sheetName = workbook.SheetNames[0];
+            const sheet = workbook.Sheets[sheetName];
 
-    // Define the file path based on the selected Kecamatan
-    let filePath = '';
+            // Parse the sheet into an array of objects
+            const excelData = XLSX.utils.sheet_to_json(sheet);
 
-    // Construct the SQL query to fetch the file path for the selected Kecamatan
-    const sql = `SELECT file FROM desa WHERE name = '${selectedDesa}'`;
-
-    // Query your database to get the file path
-    dbConnection.query(sql, (err, rows) => {
-      if (err) {
-        console.error('Error fetching file path:', err);
-        return;
-      }
-
-      print(rows, 3)
-    });
-  } else {
-    // Clear the table when the checkbox is unchecked
-    const tableBody = document.querySelector('#excelDataTable');
-    tableBody.innerHTML = ''; // Remove all table rows
-  }
-});
-
-const kelurahanCheckbox = document.getElementById('kelurahanCheckbox');
-kelurahanCheckbox.addEventListener('click', () => {
-  if (kelurahanCheckbox.checked) {
-    const selectedDesa = document.getElementById('desaDropdown').value;
-
-    // Define the file path based on the selected Kecamatan
-    let filePath = '';
-
-    // Construct the SQL query to fetch the file path for the selected Kecamatan
-    const sql = `SELECT file FROM desa WHERE name = '${selectedDesa}'`;
-
-    // Query your database to get the file path
-    dbConnection.query(sql, (err, rows) => {
-      if (err) {
-        console.error('Error fetching file path:', err);
-        return;
-      }
-
-      print(rows, 4)
-    });
-  } else {
-    // Clear the table when the checkbox is unchecked
-    const tableBody = document.querySelector('#excelDataTable');
-    tableBody.innerHTML = ''; // Remove all table rows
-  }
-});
-
-const rtCheckbox = document.getElementById('rtCheckbox');
-rtCheckbox.addEventListener('click', () => {
-  if (rtCheckbox.checked) {
-    const selectedDesa = document.getElementById('desaDropdown').value;
-
-    // Define the file path based on the selected Kecamatan
-    let filePath = '';
-
-    // Construct the SQL query to fetch the file path for the selected Kecamatan
-    const sql = `SELECT file FROM desa WHERE name = '${selectedDesa}'`;
-
-    // Query your database to get the file path
-    dbConnection.query(sql, (err, rows) => {
-      if (err) {
-        console.error('Error fetching file path:', err);
-        return;
-      }
-
-      print(rows, 5)
-    });
-  } else {
-    // Clear the table when the checkbox is unchecked
-    const tableBody = document.querySelector('#excelDataTable');
-    tableBody.innerHTML = ''; // Remove all table rows
-  }
-});
-
-const rwCheckbox = document.getElementById('rwCheckbox');
-rwCheckbox.addEventListener('click', () => {
-  if (rwCheckbox.checked) {
-    const selectedDesa = document.getElementById('desaDropdown').value;
-
-    // Define the file path based on the selected Kecamatan
-    let filePath = '';
-
-    // Construct the SQL query to fetch the file path for the selected Kecamatan
-    const sql = `SELECT file FROM desa WHERE name = '${selectedDesa}'`;
-
-    // Query your database to get the file path
-    dbConnection.query(sql, (err, rows) => {
-      if (err) {
-        console.error('Error fetching file path:', err);
-        return;
-      }
-
-      // Check if a result was found
-      print(rows, 6)
-    });
-  } else {
-    // Clear the table when the checkbox is unchecked
-    const tableBody = document.querySelector('#excelDataTable');
-    tableBody.innerHTML = ''; // Remove all table rows
-  }
-});
-
-const tpsCheckbox = document.getElementById('tpsCheckbox');
-tpsCheckbox.addEventListener('click', () => {
-  if (tpsCheckbox.checked) {
-    const selectedDesa = document.getElementById('desaDropdown').value;
-
-    // Define the file path based on the selected Kecamatan
-    let filePath = '';
-
-    // Construct the SQL query to fetch the file path for the selected Kecamatan
-    const sql = `SELECT file FROM desa WHERE name = '${selectedDesa}'`;
-
-    // Query your database to get the file path
-    dbConnection.query(sql, (err, rows) => {
-      if (err) {
-        console.error('Error fetching file path:', err);
-        return;
-      }
-
-      // Check if a result was found
-      print(rows, 7)
-    });
-  } else {
-    // Clear the table when the checkbox is unchecked
-    const tableBody = document.querySelector('#excelDataTable');
-    tableBody.innerHTML = ''; // Remove all table rows
-  }
-});
-
-function print(rows, numberData) {
-  if (rows.length > 0) {
-    const fileRow = rows[0]; // Assuming you have one row per Kecamatan
-
-    // Retrieve the file path from the database row
-    filePath = `../../database/${fileRow.file}`;
-
-    // Fetch and load the Excel file
-    fetch(filePath)
-      .then((response) => response.blob()) // Get the Excel file as a blob
-      .then((blob) => {
-        const reader = new FileReader();
-        reader.onload = (e) => {
-          const data = e.target.result;
-          const workbook = XLSX.read(data, { type: 'binary' });
-
-          // Assuming you have only one sheet in the Excel file
-          const sheetName = workbook.SheetNames[0];
-          const sheet = workbook.Sheets[sheetName];
-
-          // Parse the sheet into an array of objects
-          const excelData = XLSX.utils.sheet_to_json(sheet);
-
-          // Display the Excel data in your HTML table
-          if (numberData == 7) {
-            displayExcelData7(excelData);
-          }
-          if (numberData == 6) {
-            displayExcelData6(excelData);
-          }
-          if (numberData == 5) {
-            displayExcelData5(excelData);
-          }
-          if (numberData == 4) {
-            displayExcelData4(excelData);
-          }
-          if (numberData == 3) {
-            displayExcelData3(excelData);
-          }
-          if (numberData == 2) {
-            displayExcelData2(excelData);
-          }
-          if (numberData == 1) {
+            // Display the Excel data in your HTML table
             displayExcelData(excelData);
-          }
-        };
+          };
 
-        // Read the blob as binary data
-        reader.readAsBinaryString(blob);
-      })
-      .catch((error) => {
-        console.error('Error reading Excel file:', error);
-      });
-  } else {
-    console.error('File path not found for selected Kecamatan');
-  }
-}
+          // Read the blob as binary data
+          reader.readAsBinaryString(blob);
+        })
+        .catch((error) => {
+          console.error('Error reading Excel file:', error);
+        });
+    } else {
+      console.error('File path not found for selected Desa');
+    }
+  });
+};
+
+// Function to clear the table
+const clearTableData = () => {
+  const tableBody = document.querySelector('#excelDataTable');
+  tableBody.innerHTML = ''; // Remove all table rows
+};
 
 // Add a click event listener to the "Rekapitulasi Analisa" link
 document.getElementById('rekapitulasiLink').addEventListener('click', (event) => {
@@ -427,147 +228,55 @@ document.getElementById('close-rekapitulasi').addEventListener('click', (event) 
   analisaDataSection.style.display = 'block';
 });
 
-// Initialize a variable to keep track of the last index
-// Function to display Excel data in the HTML table without replacing existing data
+// Define an object to map column names to checkboxes
+const columnCheckboxes = {
+  Nama: document.getElementById('namaCheckbox'),
+  'Jenis Kelamin': document.getElementById('jeniskelaminCheckbox'),
+  'Usia': document.getElementById('usiaCheckbox'),
+  'Kelurahan': document.getElementById('kelurahanCheckbox'),
+  'RT': document.getElementById('rtCheckbox'),
+  'RW': document.getElementById('rwCheckbox'),
+  'TPS': document.getElementById('tpsCheckbox'),
+  // Add other column checkboxes here
+};
+
+// Add event listeners for each checkbox
+for (const column in columnCheckboxes) {
+  columnCheckboxes[column].addEventListener('click', () => {
+    const isChecked = columnCheckboxes[column].checked;
+    const selectedColumn = columnCheckboxes[column].getAttribute('data-column');
+
+    // Get all table cells in the selected column
+    const tableCells = document.querySelectorAll(`#excelDataTable td[data-column="${selectedColumn}"]`);
+
+    // Update the class of table cells based on the checkbox state
+    tableCells.forEach((cell) => {
+      if (isChecked) {
+        cell.classList.remove('not-selected');
+      } else {
+        cell.classList.add('not-selected');
+      }
+    });
+  });
+}
+
+// Function to display Excel data in the HTML table
 function displayExcelData(data) {
   const tableBody = document.querySelector('#excelDataTable');
   tableBody.innerHTML = '';
   // Iterate through the Excel data and create table rows
   data.forEach((row, index) => {
     const tableRow = document.createElement('tr');
+    // Add "data-column" attributes to table cells for mapping to checkboxes
     tableRow.innerHTML = `
       <td>${index + 1}</td>
-      <td>${row.Nama}</td>
-      <td class="not-selected">${row['Jenis Kelamin']}</td>
-      <td class="not-selected">${row.Usia}</td>
-      <td class="not-selected">${row.Kelurahan}</td>
-      <td class="not-selected">${row.RT}</td>
-      <td class="not-selected">${row.RW}</td>
-      <td class="not-selected">${row.TPS}</td>
-    `;
-
-    // Append the new row to the existing table
-    tableBody.appendChild(tableRow);
-  });
-}
-
-function displayExcelData2(data) {
-  const tableBody = document.querySelector('#excelDataTable');
-  tableBody.innerHTML = '';
-  // Iterate through the Excel data and create table rows
-  data.forEach((row, index) => {
-    const tableRow = document.createElement('tr');
-    tableRow.innerHTML = `
-    <td>${index + 1}</td>
-    <td>${row.Nama}</td>
-    <td>${row['Jenis Kelamin']}</td>
-    <td class="not-selected">${row.Usia}</td>
-    <td class="not-selected">${row.Kelurahan}</td>
-    <td class="not-selected">${row.RT}</td>
-    <td class="not-selected">${row.RW}</td>
-    <td class="not-selected">${row.TPS}</td>
-    `;
-
-    // Append the new row to the existing table
-    tableBody.appendChild(tableRow);
-  });
-}
-
-function displayExcelData3(data) {
-  const tableBody = document.querySelector('#excelDataTable');
-  tableBody.innerHTML = '';
-  // Iterate through the Excel data and create table rows
-  data.forEach((row, index) => {
-    const tableRow = document.createElement('tr');
-    tableRow.innerHTML = `
-      <td>${index + 1}</td>
-      <td>${row.Nama}</td>
-      <td>${row['Jenis Kelamin']}</td>
-      <td>${row.Usia}</td>
-      <td class="not-selected">${row.Kelurahan}</td>
-      <td class="not-selected">${row.RT}</td>
-      <td class="not-selected">${row.RW}</td>
-      <td class="not-selected">${row.TPS}</td>
-    `;
-    tableBody.appendChild(tableRow);
-  });
-}
-
-function displayExcelData4(data) {
-  const tableBody = document.querySelector('#excelDataTable');
-  tableBody.innerHTML = '';
-  // Iterate through the Excel data and create table rows
-  data.forEach((row, index) => {
-    const tableRow = document.createElement('tr');
-    tableRow.innerHTML = `
-      <td>${index + 1}</td>
-      <td>${row.Nama}</td>
-      <td>${row['Jenis Kelamin']}</td>
-      <td>${row.Usia}</td>
-      <td>${row.Kelurahan}</td>
-      <td class="not-selected">${row.RT}</td>
-      <td class="not-selected">${row.RW}</td>
-      <td class="not-selected">${row.TPS}</td>
-    `;
-    tableBody.appendChild(tableRow);
-  });
-}
-
-function displayExcelData5(data) {
-  const tableBody = document.querySelector('#excelDataTable');
-  tableBody.innerHTML = '';
-  // Iterate through the Excel data and create table rows
-  data.forEach((row, index) => {
-    const tableRow = document.createElement('tr');
-    tableRow.innerHTML = `
-      <td>${index + 1}</td>
-      <td>${row.Nama}</td>
-      <td>${row['Jenis Kelamin']}</td>
-      <td>${row.Usia}</td>
-      <td>${row.Kelurahan}</td>
-      <td>${row.RT}</td>
-      <td class="not-selected">${row.RW}</td>
-      <td class="not-selected">${row.TPS}</td>
-    `;
-    tableBody.appendChild(tableRow);
-  });
-}
-
-function displayExcelData6(data) {
-  const tableBody = document.querySelector('#excelDataTable');
-  tableBody.innerHTML = '';
-  // Iterate through the Excel data and create table rows
-  data.forEach((row, index) => {
-    const tableRow = document.createElement('tr');
-    tableRow.innerHTML = `
-      <td>${index + 1}</td>
-      <td>${row.Nama}</td>
-      <td>${row['Jenis Kelamin']}</td>
-      <td>${row.Usia}</td>
-      <td>${row.Kelurahan}</td>
-      <td>${row.RT}</td>
-      <td>${row.RW}</td>
-      <td class="not-selected">${row.TPS}</td>
-    `;
-    tableBody.appendChild(tableRow);
-  });
-}
-
-function displayExcelData7(data) {
-  const tableBody = document.querySelector('#excelDataTable');
-  tableBody.innerHTML = '';
-  // Iterate through the Excel data and create table rows
-  data.forEach((row, index) => {
-    const tableRow = document.createElement('tr');
-    tableRow.innerHTML = `
-      <td>${index + 1}</td>
-      <td>${row.Nama}</td>
-      <td>${row['Jenis Kelamin']}</td>
-      <td>${row.Usia}</td>
-      <td>${row.Kelurahan}</td>
-      <td>${row.RT}</td>
-      <td>${row.RW}</td>
-      <td>${row.TPS}</td>
+      <td data-column="Nama" class="not-selected">${row.Nama}</td>
+      <td data-column="Jenis Kelamin" class="not-selected">${row['Jenis Kelamin']}</td>
+      <td data-column="Usia" class="not-selected">${row.Usia}</td>
+      <td data-column="Kelurahan" class="not-selected">${row.Kelurahan}</td>
+      <td data-column="RT" class="not-selected">${row.RT}</td>
+      <td data-column="RW" class="not-selected">${row.RW}</td>
+      <td data-column="TPS" class="not-selected">${row.TPS}</td>
     `;
     tableBody.appendChild(tableRow);
   });
